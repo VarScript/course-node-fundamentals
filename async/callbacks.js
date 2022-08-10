341d44f701c0cdd27b71ad94a0d7a5c2c1901152
DevHars <vargassanchezfabio01@gmail.com>
2022-08-10 10:53:13 -0500

    callbacks

---
+++ async/callbacks.js
@@ -0,0 +1,28 @@
+function hi(name, myCallback) {
+    setTimeout(function () {
+        console.log('Hi, ' + name);
+        myCallback();
+        
+    }, 1000)
+}
+
+function bye(name, otherCallback) {
+    setTimeout(function () {
+        console.log('bye,', name);
+        otherCallback();
+    }) 
+
+}
+
+console.log('Starting process...');
+
+hi('Fabio', function () {
+    bye('Fabito', function () {
+        console.log('Ending process...');
+    });
+    
+});
+
+// hi('Fabio', function() {});
+// bye('Fabio', function() {});
+
