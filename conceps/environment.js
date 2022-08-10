af03dfbeb2ab06c2ddb24b8fc080ff819a95687c
DevHars <vargassanchezfabio01@gmail.com>
2022-08-09 22:04:08 -0500

    environment variables

---
+++ conceps/environment.js
@@ -0,0 +1,8 @@
+// Variables de entorno
+
+let names = process.env.NAMESED || 'no name';
+let web = process.env.WEB || 'i dont web'
+
+console.log('Hi ' + names);
+console.log('My web its: ' + web);
+
