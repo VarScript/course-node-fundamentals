e3b99341386a3cf8b30ed3f8bd1e0333351288e9
DevHars <vargassanchezfabio01@gmail.com>
2022-08-09 21:45:49 -0500

    monowire concept

---
+++ conceps/monohilo.js
@@ -0,0 +1,14 @@
+console.log('Hellow world');
+
+let i = 0;
+setInterval(function() {
+    console.log (i);
+    i++;
+    /*
+    if (i === 5) var a = 3 + z;
+    */
+
+    //console.log('Active')
+}, 1000);
+
+console.log('Second instruction');
