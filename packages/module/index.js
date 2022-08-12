2641cea4334a11d30d35cafc12e724003d39a2fe
DevHars <vargassanchezfabio01@gmail.com>
2022-08-12 11:09:07 -0500

    modules req and modules es6

---
+++ packages/module/index.js
@@ -0,0 +1,9 @@
+// Bring our module
+
+module = require('./module');
+
+
+// Run a function of the module
+
+console.log(module.prop1);
+module.greet()
---
+++ packages/module/module.js
@@ -0,0 +1,8 @@
+function greet() {
+    console.log('Hello world!');
+};
+
+module.exports = {
+    greet,
+    prop1: 'Hi, how are you?'
+};
