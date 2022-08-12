936da8afe2f95d5e2f41a523179766a2f21b1b03
DevHars <vargassanchezfabio01@gmail.com>
2022-08-11 21:13:18 -0500

    way to create native modules in c++

---
+++ modules/native/build/Release/.deps/Release/obj.target/addon/hi.o.d
@@ -0,0 +1,21 @@
+cmd_Release/obj.target/addon/hi.o := g++ -o Release/obj.target/addon/hi.o ../hi.cc '-DNODE_GYP_MODULE_NAME=addon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D__STDC_FORMAT_MACROS' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DBUILDING_NODE_EXTENSION' -I/home/devhars/.cache/node-gyp/14.19.3/include/node -I/home/devhars/.cache/node-gyp/14.19.3/src -I/home/devhars/.cache/node-gyp/14.19.3/deps/openssl/config -I/home/devhars/.cache/node-gyp/14.19.3/deps/openssl/openssl/include -I/home/devhars/.cache/node-gyp/14.19.3/deps/uv/include -I/home/devhars/.cache/node-gyp/14.19.3/deps/zlib -I/home/devhars/.cache/node-gyp/14.19.3/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/addon/hi.o.d.raw   -c
+Release/obj.target/addon/hi.o: ../hi.cc \
+ /home/devhars/.cache/node-gyp/14.19.3/include/node/node.h \
+ /home/devhars/.cache/node-gyp/14.19.3/include/node/v8.h \
+ /home/devhars/.cache/node-gyp/14.19.3/include/node/cppgc/common.h \
+ /home/devhars/.cache/node-gyp/14.19.3/include/node/v8config.h \
+ /home/devhars/.cache/node-gyp/14.19.3/include/node/v8-internal.h \
+ /home/devhars/.cache/node-gyp/14.19.3/include/node/v8-version.h \
+ /home/devhars/.cache/node-gyp/14.19.3/include/node/v8config.h \
+ /home/devhars/.cache/node-gyp/14.19.3/include/node/v8-platform.h \
+ /home/devhars/.cache/node-gyp/14.19.3/include/node/node_version.h
+../hi.cc:
+/home/devhars/.cache/node-gyp/14.19.3/include/node/node.h:
+/home/devhars/.cache/node-gyp/14.19.3/include/node/v8.h:
+/home/devhars/.cache/node-gyp/14.19.3/include/node/cppgc/common.h:
+/home/devhars/.cache/node-gyp/14.19.3/include/node/v8config.h:
+/home/devhars/.cache/node-gyp/14.19.3/include/node/v8-internal.h:
+/home/devhars/.cache/node-gyp/14.19.3/include/node/v8-version.h:
+/home/devhars/.cache/node-gyp/14.19.3/include/node/v8config.h:
+/home/devhars/.cache/node-gyp/14.19.3/include/node/v8-platform.h:
+/home/devhars/.cache/node-gyp/14.19.3/include/node/node_version.h:
