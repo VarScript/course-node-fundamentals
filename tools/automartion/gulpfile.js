b7eb03c17ebedac311be9a398b5114bee09f3478
DevHars <vargassanchezfabio01@gmail.com>
2022-08-16 11:13:30 -0500

    gulp

---
+++ tools/automartion/gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const server = require('gulp-server-livereload')
+
+gulp.task('build', function(cb) {
+    console.log('Building the site');
+    setTimeout(cb, 1200);
+});
+
+gulp.task('serve', function(cb) {
+    gulp.src('www')
+        .pipe(server({
+            livereload: true,
+            open: true,
+        }));
+})
+
